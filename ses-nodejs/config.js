802cf7c73cc8368cc7f4b25b415a726bc24a0b4b
Eduard <eduard.duarte@hotmail.com>
2020-02-29 21:17:19 -0500

    ses dos

---
+++ ses-nodejs/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    'aws': {
+        'key': 'YOUR_ACCESS_KEY',
+        'secret': 'YOUR_ACCESS_SECRET',
+        'ses': {
+            'from': {
+                // replace with actual email address
+                'default': '"Eduard.Duarte" <e.duartes@uniandes.edu.co>',
+            },
+            // e.g. us-west-2
+            'region': 'us-east-1' 
+        }
+    }
+};
